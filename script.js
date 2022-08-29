fd5d32658045c4eea0d7c64bf4d3bbb2cbe07194
Allan <allanzissimo@gmail.com>
2022-08-29 16:07:02 -0300

    análise com condições

--- script.js
+++ script.js
@@ -9,14 +9,34 @@ const calculo = () => {
     var pesoTexto = peso.value;
     var altura = document.querySelector("#altura");
     var alturaTexto = altura.value;
+    var classifica = "";
 
-    var imc = pesoTexto / (alturaTexto * 2)
+    var imc = pesoTexto / (alturaTexto * 2);
 
     document.write("Olá, " + nomeTexto)
     linha()
-    document.write("Seu peso é: " + pesoTexto)
+    document.write("Seu peso é: " + pesoTexto + "Kg")
     linha()
-    document.write("Sua altura é: " + alturaTexto)
+    document.write("Sua altura é: " + alturaTexto + "m")
     linha()
-    document.write(" Seu IMC é de: " + imc.toFixed())
+    document.write(" Seu IMC é de: " + imc.toFixed(2))
+
+    if(imc = 18 && imc < 25){
+        classifica = "Normal"
+        linha()
+        document.write("Sua classificação é: " + classifica);
+    }else if(imc = 25 && imc < 30){
+        classifica = "Sobrepeso"
+        linha()
+        document.write("Sua classificação é: " + classifica);
+    }else if(imc = 30 && imc < 40){
+        classifica = "Obesidade"
+        linha()
+        document.write("Sua classificação é: " + classifica);
+    }else if(imc > 40){
+        classifica = "Obesidade grave"
+        linha()
+        document.write("Sua classificação é: " + classifica);
+    }
+    
 }
